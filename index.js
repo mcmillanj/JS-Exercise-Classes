5deaf78d4175d3adeb814a0f9eac4bfe3fdb21bd
therman mcmillan <mcmillanj0106@yahoo.com>
2021-09-16 15:34:00 -0400

    54 passed

--- index.js
+++ index.js
@@ -220,8 +220,22 @@ sprintChallenge(subject)
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-   
+class ProjectManager extends Instructor
+{
+ constructor(attr)
+ {
+  super(attr);
+  this.gradClassName = attr.gradClassName;
+  this.favInstructor = attr.favInstructor;
+ }  
+ standUp(subject)
+ {
+   return `${this.name} announces to ${subject}, @channel standy times!`;
+ }
+ debugsCode(student,subject)
+ {
+   return `${this.name} debugs ${student.name}'s code on ${subject}`
+ }
 }
 /*
   STRETCH PROBLEM (no tests!)
